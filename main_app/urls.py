4c25205be662977d2fc3d675b5f2321bb4fce8b1
Yeani <ifeanyi200019@gmail.com>
2024-04-09 16:58:33 -0400

    Added login and signup options

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('dogs/create', views.DogCreate.as_view(), name='dogs_create'),
     path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete'),
     path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
+    path('accounts/signup/', views.signup, name='signup'),
 
 ]
