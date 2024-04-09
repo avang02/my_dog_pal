f4cb764a99fee045c50a9ca4993a657b0eda2806
Stephen Nguyen <snguyen@steven.attlocal.net>
2024-04-09 15:08:22 -0500

    signup authentication

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import request
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Dog
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -31,3 +33,19 @@ class DogDelete(DeleteView):
 class DogUpdate(UpdateView):
     model = Dog
     fields = '__all__'
+
+def signup(requeset):
+    error_message=''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
