2d6f0f19b00f5ad96f780cebc9ad9f943a6854eb
scooterpops <rjvicenzi@gmail.com>
2024-04-09 09:49:52 -0400

    created dog model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Dog(models.Model):
+    name = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50)
+    neutered_sprayed = models.BooleanField(default=False)
+    weight = models.FloatField()
+    birthdate = models.DateField(null=True, blank=True)
+    
+    def __string__(self):
+        return self.name
+    
+
+
+
+
