8e8384319412b4b4523943dd6ff01ab8c0716c1c
Stephen Nguyen <snguyen@steven.attlocal.net>
2024-04-09 09:30:43 -0500

    added DogCreate class, made forms, and UI

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import request
+from django.views.generic.edit import CreateView
+from .models import Dog
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog.id':self.id})
